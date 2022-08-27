d064c0b1d4149ab370807d9bad148974dc01bcfd
Atul Mane <maneatul225@gmail.com>
2022-08-27 19:30:14 +0530

    nvim-tree setup

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -1,6 +1,12 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  use {
+  'kyazdani42/nvim-tree.lua',
+   requires = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icons
+  },
+}
 end)
---
+++ plugin/nvim_tree.lua
@@ -0,0 +1,53 @@
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    highlight_git = true,
+    group_empty = true,
+    icons = {
+      show = {
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+
+      }
+    }
+  },
+  filters = {
+    dotfiles = true,
+  },
+  diagnostics = {
+    enable = true,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+})
