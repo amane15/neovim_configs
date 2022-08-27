aebdcb8b33c1b30fa7181d1494cd95b1fb8cc9a2
Atul Mane <maneatul225@gmail.com>
2022-08-27 20:55:15 +0530

    lualine setup

--- lua/packer_plugins.lua
+++ lua/packer_plugins.lua
@@ -30,4 +30,7 @@ return require('packer').startup(function(use)
     use 'L3MON4D3/LuaSnip'
     use 'saadparwaiz1/cmp_luasnip'
 
+    use 'nvim-lualine/lualine.nvim}'
+
+
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,11 @@ _G.packer_plugins = {
     path = "/home/atul/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/atul/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/home/atul/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
